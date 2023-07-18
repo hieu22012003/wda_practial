978ea5e4981119bab3784f9b69621631b257be29
hieuelcom <112467458+hieuelcom@users.noreply.github.com>
2023-07-18 09:43:17 +0700

    first commit

---
+++ THcode/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace THcode.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
